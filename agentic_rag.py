e61a5452b37722fbbe5ee27cb23b896cc9a686ce
SHNacho <nachosanchezh@hotmail.com>
2024-10-01 15:59:04 +0200

    feat(sysprompt): Include some system prompts for better beahviour

--- agentic_rag.py
+++ agentic_rag.py
@@ -21,7 +21,6 @@ files = ["data/my-cv.md", "data/my-hobbies.md"]
 
 @st.cache_resource(ttl="1h")
 def configure_retriever(files):
-    print("Embedding")
     # Read documents
     docs = []
     for file_path in files:
@@ -29,7 +28,7 @@ def configure_retriever(files):
         docs.extend(loader.load())
 
     # Split documents
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=200)
+    text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(chunk_size=500, chunk_overlap=0)
     splits = text_splitter.split_documents(docs)
 
     vectorstore = Chroma.from_documents(
@@ -59,7 +58,7 @@ tools = [retriever_tool]
 from typing import Annotated, Sequence
 from typing_extensions import TypedDict
 
-from langchain_core.messages import AIMessage, BaseMessage
+from langchain_core.messages import BaseMessage
 
 from langgraph.graph.message import add_messages
 
@@ -75,8 +74,7 @@ from typing import Annotated, Literal, Sequence
 from typing_extensions import TypedDict
 
 from langchain import hub
-from langchain_core.messages import BaseMessage, HumanMessage
-from langchain_core.output_parsers import StrOutputParser
+from langchain_core.messages import BaseMessage
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 
@@ -126,7 +124,7 @@ def grade_documents(state) -> Literal["generate", "rewrite"]:
     messages = state["messages"]
     last_message = messages[-1]
 
-    question = messages[0].content
+    question = messages[1].content
     docs = last_message.content
 
     scored_result = chain.invoke({"question": question, "context": docs})
@@ -154,8 +152,16 @@ def agent(state):
     Returns:
         dict: The updated state with the agent response appended to messages
     """
+    system = {
+        "role": "system",
+        "content": (
+            "You are Ignacio's assistant. You only answer question about him."
+        )
+    }
     messages = state["messages"]
-    model = ChatOpenAI(temperature=0, streaming=True, model="gpt-4o-mini")
+    messages.insert(0, system)
+
+    model = ChatOpenAI(temperature=0.4, streaming=True, model="gpt-4o-mini")
     model = model.bind_tools(tools)
     response = model.invoke(messages)
     # We return a list, because this will get added to the existing list
@@ -173,16 +179,19 @@ def generate(state):
          dict: The updated state with re-phrased question
     """
     messages = state["messages"]
-    question = messages[0].content
+    print(messages)
+    question = messages[1].content
     last_message = messages[-1]
+    print("Question: ", question)
 
     docs = last_message.content
+    print(docs)
 
     # Prompt
     prompt = hub.pull("rlm/rag-prompt")
 
     # LLM
-    llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0, streaming=True)
+    llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0.4, streaming=True)
 
     # Chain
     rag_chain = prompt | llm
@@ -228,47 +237,35 @@ workflow.add_edge("generate", END)
 graph = workflow.compile()
 
 
-#inputs = {
-#    "messages": [
-#        ("user", "Hi!")
-#    ]
-#}
-#for output in graph.stream(inputs):
-#    for key, value in output.items():
-#        pprint.pprint(f"Output from node '{key}':")
-#        pprint.pprint("---")
-#        pprint.pprint(value, indent=2, width=80, depth=None)
-#    pprint.pprint("\n---\n")
-
 ##### Streamlit Chat Interface #####
 def display_chat():
+    avatars = {"user": "user", "bot": "assistant"}
+
+    if len(st.session_state.messages) == 0:
+        st.session_state.messages.append(
+            ("bot", "Hi! I'm Ignacio's assistant, how can I help you?")
+        )
+    
     # Display previous messages
     for role, content in st.session_state.messages:
-        if role == "user":
-            st.write(f"**User:** {content}")
-        else:
-            st.write(f"**Bot:** {content}")
+        st.chat_message(avatars[role]).write(content)
 
     # Input box for user query
-    user_input = st.chat_input("Ask Ignacio a question:")
+    user_input = st.chat_input("Ask anything about Ignacio:")
     if user_input:
         st.session_state.messages.append(("user", user_input))
-        st.write(f"**User:** {user_input}")
+        st.chat_message("user").write(user_input)
 
         inputs = {"messages": [("user", user_input)]}
-        print("-------New------")
         for output in graph.stream(inputs):
             for key, value in output.items():
-                print("Key: ",key)
-                print("Value: ",value)
-                print('----------------')
                 message = value["messages"][0]
                 if key == "generate": #or key == "agent":
                     st.session_state.messages.append(("bot", message.content))
-                    st.write(f"**Bot:** {message.content}")
+                    st.chat_message("assistant").write(message.content)
                 elif key == "agent" and message.content != '':
                     st.session_state.messages.append(("bot", message.content))
-                    st.write(f"**Bot:** {message.content}")
+                    st.chat_message("assistant").write(message.content)
 
 
 # Call the display_chat function to render the chat
